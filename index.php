0844a4efcb5b0190c3ba34e974b86ef1cfccd73d
dineshlkg <dineshbabu@spinzsoft.com>
2025-10-07 13:03:38 +0530

    Feat : new menu link added in top navigation menu

--- index.php
+++ index.php
@@ -58,6 +58,7 @@
         <li class="active"><a href="#">Home</a></li>
         <li><a href="#">About</a></li>
         <li><a href="#">Projects</a></li>
+        <li><a href="#">Gallery</a></li>
         <li><a href="#">Contact</a></li>
       </ul>
       <ul class="nav navbar-nav navbar-right">
